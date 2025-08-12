35a69ab0a2dbc72dc2348a30e6b3378c7b4e3a25
Nico-DM <nicomgaletto@gmail.com>
2025-08-12 20:09:09 +0000

    implemented gemini

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
+import os
+from dotenv import load_dotenv
+from google import genai
+
+
 def main():
-    print("Hello from aiagent!")
+    load_dotenv()
+    api_key = os.environ.get("GEMINI_API_KEY")
+    client = genai.Client(api_key=api_key)
+    prompt = "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+    response = client.models.generate_content(model="gemini-2.0-flash-001", contents=prompt)
+    print(response.text)
+    print("Prompt tokens:", response.usage_metadata.prompt_token_count) # type: ignore
+    print("Response tokens:", response.usage_metadata.candidates_token_count) # type: ignore
 
 
 if __name__ == "__main__":
