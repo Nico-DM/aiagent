673a12e0a600e8c715d81e8e8a4baeb6e0270767
Nico-DM <nicomgaletto@gmail.com>
2025-08-14 23:01:13 +0000

    added context support

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os, sys
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
 
 def main():
@@ -8,10 +9,16 @@ def main():
         print("Error: missing prompt")
         sys.exit(1)
     prompt = sys.argv[1]
+    messages = [
+        types.Content(role="user", parts=[types.Part(text=prompt)]),
+    ]
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key=api_key)
-    response = client.models.generate_content(model="gemini-2.0-flash-001", contents=prompt)
+    response = client.models.generate_content(
+        model="gemini-2.0-flash-001",
+        contents=messages
+    )
     print(response.text)
     print("Prompt tokens:", response.usage_metadata.prompt_token_count) # type: ignore
     print("Response tokens:", response.usage_metadata.candidates_token_count) # type: ignore
