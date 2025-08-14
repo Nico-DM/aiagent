e89f6449dc23ae0f6e815f6cda191e0d4ca00021
Nico-DM <nicomgaletto@gmail.com>
2025-08-14 01:47:09 +0000

    added input

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
-import os
+import os, sys
 from dotenv import load_dotenv
 from google import genai
 
 
 def main():
+    if len(sys.argv) < 2:
+        print("Error: missing prompt")
+        sys.exit(1)
+    prompt = sys.argv[1]
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key=api_key)
-    prompt = "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
     response = client.models.generate_content(model="gemini-2.0-flash-001", contents=prompt)
     print(response.text)
     print("Prompt tokens:", response.usage_metadata.prompt_token_count) # type: ignore
